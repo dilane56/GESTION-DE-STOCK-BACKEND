a5395efa76d1e0929b37e0f80262f3d2f3cd741c
dilane56 <kamgaingfotso@gmail.com>
2025-04-14 18:38:13 +0100

    project initialisation

---
+++ src/main/java/org/kfokam48/gestiondestockbackend/GestionDeStockBackendApplication.java
@@ -0,0 +1,13 @@
+package org.kfokam48.gestiondestockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDeStockBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kfokam48/gestiondestockbackend/GestionDeStockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kfokam48.gestiondestockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeStockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
